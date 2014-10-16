d5be690f3bed087bef264f8c2c4fb61492cbf3e9
Thomas W <netzberufler@googlemail.com>
2014-10-16 10:29:14 +0200

    added new panel to customizer and moved all sections and settings to it

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -7,20 +7,32 @@
 // Load Customizer Helper Functions
 require( get_template_directory() . '/inc/customizer/customizer-functions.php' );
 
+// Load Customizer Settings
+require( get_template_directory() . '/inc/customizer/customizer-header.php' );
+require( get_template_directory() . '/inc/customizer/customizer-post.php' );
+require( get_template_directory() . '/inc/customizer/customizer-slider.php' );
+require( get_template_directory() . '/inc/customizer/customizer-upgrade.php' );
+
+
 // Add Theme Options section to Customizer
 add_action( 'customize_register', 'smartline_customize_register_options' );
 
 function smartline_customize_register_options( $wp_customize ) {
 
+	// Add Theme Options Panel
+	$wp_customize->add_panel( 'smartline_options_panel', array(
+		'priority'       => 180,
+		'capability'     => 'edit_theme_options',
+		'theme_supports' => '',
+		'title'          => __( 'Theme Options', 'smartlinelite' ),
+		'description'    => '',
+	) );
+
 	// Add Section for Theme Options
-	$wp_customize->add_section( 'smartline_section_options', array(
-        'title'    => __( 'Theme Options', 'smartline-lite' ),
-        'priority' => 180
-		)
-	);
-	$wp_customize->add_section( 'smartline_section_upgrade', array(
-        'title'    => __( 'PRO Version', 'smartline-lite' ),
-        'priority' => 190
+	$wp_customize->add_section( 'smartline_section_general', array(
+        'title'    => __( 'General Settings', 'smartlinelite' ),
+        'priority' => 10,
+		'panel' => 'smartline_options_panel' 
 		)
 	);
 	
@@ -34,7 +46,7 @@ function smartline_customize_register_options( $wp_customize ) {
 	);
     $wp_customize->add_control( 'smartline_control_layout', array(
         'label'    => __( 'Theme Layout', 'smartline-lite' ),
-        'section'  => 'smartline_section_options',
+        'section'  => 'smartline_section_general',
         'settings' => 'smartline_theme_options[layout]',
         'type'     => 'radio',
 		'priority' => 1,
@@ -44,240 +56,6 @@ function smartline_customize_register_options( $wp_customize ) {
 			)
 		)
 	);
-
-	// Add Header Content Headline
-	$wp_customize->add_setting( 'smartline_theme_options[header_content]', array(
-        'default'           => '',
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        )
-    );
-    $wp_customize->add_control( new Smartline_Customize_Header_Control(
-        $wp_customize, 'smartline_control_header_content', array(
-            'label' => __( 'Header Content', 'smartline-lite' ),
-            'section' => 'smartline_section_options',
-            'settings' => 'smartline_theme_options[header_content]',
-            'priority' => 	3
-            )
-        )
-    );
-	$wp_customize->add_setting( 'smartline_theme_options[header_content_description]', array(
-        'default'           => '',
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        )
-    );
-    $wp_customize->add_control( new Smartline_Customize_Description_Control(
-        $wp_customize, 'smartline_control_header_content_description', array(
-            'label' =>  __( 'The Header Content configured below will be displayed on the right hand side of the header area.', 'smartline-lite' ),
-            'section' => 'smartline_section_options',
-            'settings' => 'smartline_theme_options[header_content_description]',
-            'priority' => 	4,
-			'description' =>  __( 'Stay hungry. Stay foolish.', 'smartline-lite' )
-            )
-        )
-    );
-
-	// Add Settings and Controls for Header
-	$wp_customize->add_setting( 'smartline_theme_options[header_search]', array(
-        'default'           => false,
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'smartline_sanitize_checkbox'
-		)
-	);
-    $wp_customize->add_control( 'smartline_control_header_search', array(
-        'label'    => __( 'Display search field on header area', 'smartline-lite' ),
-        'section'  => 'smartline_section_options',
-        'settings' => 'smartline_theme_options[header_search]',
-        'type'     => 'checkbox',
-		'priority' => 6
-		)
-	);
-
-	$wp_customize->add_setting( 'smartline_theme_options[header_icons]', array(
-        'default'           => false,
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'smartline_sanitize_checkbox'
-		)
-	);
-    $wp_customize->add_control( 'smartline_control_header_icons', array(
-        'label'    => __( 'Display Social Icons on header area', 'smartline-lite' ),
-        'section'  => 'smartline_section_options',
-        'settings' => 'smartline_theme_options[header_icons]',
-        'type'     => 'checkbox',
-		'priority' => 7
-		)
-	);
-
-	// Add Settings and Controls for Posts
-	$wp_customize->add_setting( 'smartline_theme_options[posts_length]', array(
-        'default'           => 'index',
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'smartline_sanitize_post_length'
-		)
-	);
-    $wp_customize->add_control( 'smartline_control_posts_length', array(
-        'label'    => __( 'Post Length on archives', 'smartline-lite' ),
-        'section'  => 'smartline_section_options',
-        'settings' => 'smartline_theme_options[posts_length]',
-        'type'     => 'radio',
-		'priority' => 8,
-        'choices'  => array(
-            'index' => __( 'Show full posts', 'smartline-lite' ),
-            'excerpt' => __( 'Show post summaries (excerpt)', 'smartline-lite' )
-			)
-		)
-	);
-
-	$wp_customize->add_setting( 'smartline_theme_options[post_thumbnails_index]', array(
-        'default'           => true,
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'smartline_sanitize_checkbox'
-		)
-	);
-    $wp_customize->add_control( 'smartline_control_posts_thumbnails_index', array(
-        'label'    => __( 'Display featured images on archive pages', 'smartline-lite' ),
-        'section'  => 'smartline_section_options',
-        'settings' => 'smartline_theme_options[post_thumbnails_index]',
-        'type'     => 'checkbox',
-		'priority' => 9
-		)
-	);
-
-	$wp_customize->add_setting( 'smartline_theme_options[post_thumbnails_single]', array(
-        'default'           => true,
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'smartline_sanitize_checkbox'
-		)
-	);
-    $wp_customize->add_control( 'smartline_control_posts_thumbnails_single', array(
-        'label'    => __( 'Display featured images on single posts', 'smartline-lite' ),
-        'section'  => 'smartline_section_options',
-        'settings' => 'smartline_theme_options[post_thumbnails_single]',
-        'type'     => 'checkbox',
-		'priority' => 10
-		)
-	);
-	
-	// Add settings and controls for Post Slider
-	$wp_customize->add_setting( 'smartline_theme_options[slider_activated]', array(
-        'default'           => '',
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        )
-    );
-    $wp_customize->add_control( new Smartline_Customize_Header_Control(
-        $wp_customize, 'smartline_control_slider_activated', array(
-            'label' => __( 'Activate Featured Post Slider', 'smartline-lite' ),
-            'section' => 'smartline_section_options',
-            'settings' => 'smartline_theme_options[slider_activated]',
-            'priority' => 	12
-            )
-        )
-    );
-	$wp_customize->add_setting( 'smartline_theme_options[slider_activated_front_page]', array(
-        'default'           => false,
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'smartline_sanitize_checkbox'
-		)
-	);
-    $wp_customize->add_control( 'smartline_control_slider_activated_frontpage', array(
-        'label'    => __( 'Display Slider on Magazine Front Page template.', 'smartline-lite' ),
-        'section'  => 'smartline_section_options',
-        'settings' => 'smartline_theme_options[slider_activated_front_page]',
-        'type'     => 'checkbox',
-		'priority' => 13
-		)
-	);
-	$wp_customize->add_setting( 'smartline_theme_options[slider_activated_blog]', array(
-        'default'           => false,
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'smartline_sanitize_checkbox'
-		)
-	);
-    $wp_customize->add_control( 'smartline_control_slider_activated_blog', array(
-        'label'    => __( 'Display Slider on normal blog index.', 'smartline-lite' ),
-        'section'  => 'smartline_section_options',
-        'settings' => 'smartline_theme_options[slider_activated_blog]',
-        'type'     => 'checkbox',
-		'priority' => 14
-		)
-	);
-
-	$wp_customize->add_setting( 'smartline_theme_options[slider_animation]', array(
-        'default'           => 'horizontal',
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        'sanitize_callback' => 'smartline_sanitize_slider_animation'
-		)
-	);
-    $wp_customize->add_control( 'smartline_control_slider_animation', array(
-        'label'    => __( 'Slider Animation', 'smartline-lite' ),
-        'section'  => 'smartline_section_options',
-        'settings' => 'smartline_theme_options[slider_animation]',
-        'type'     => 'radio',
-		'priority' => 15,
-        'choices'  => array(
-            'horizontal' => __( 'Horizontal Slider', 'smartline-lite' ),
-            'fade' => __( 'Fade Slider', 'smartline-lite' )
-			)
-		)
-	);
-	
-	
-	// Add PRO Version Section
-	$wp_customize->add_setting( 'smartline_theme_options[pro_version_label]', array(
-        'default'           => '',
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        )
-    );
-    $wp_customize->add_control( new Smartline_Customize_Header_Control(
-        $wp_customize, 'smartline_control_pro_version_label', array(
-            'label' => __( 'Need more features?', 'smartline-lite' ),
-            'section' => 'smartline_section_upgrade',
-            'settings' => 'smartline_theme_options[pro_version_label]',
-            'priority' => 	1
-            )
-        )
-    );
-	$wp_customize->add_setting( 'smartline_theme_options[pro_version]', array(
-        'default'           => '',
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        )
-    );
-    $wp_customize->add_control( new Smartline_Customize_Text_Control(
-        $wp_customize, 'smartline_control_pro_version', array(
-            'label' =>  __( 'Check out the PRO version which comes with additional features and advanced customization options.', 'smartline-lite' ),
-            'section' => 'smartline_section_upgrade',
-            'settings' => 'smartline_theme_options[pro_version]',
-            'priority' => 	2
-            )
-        )
-    );
-	$wp_customize->add_setting( 'smartline_theme_options[pro_version_button]', array(
-        'default'           => '',
-		'type'           	=> 'option',
-        'transport'         => 'refresh',
-        )
-    );
-    $wp_customize->add_control( new Smartline_Customize_Button_Control(
-        $wp_customize, 'smartline_control_pro_version_button', array(
-            'label' => __('Learn more about the PRO Version', 'smartline-lite'),
-			'section' => 'smartline_section_upgrade',
-            'settings' => 'smartline_theme_options[pro_version_button]',
-            'priority' => 	3
-            )
-        )
-    );
 	
 	// Add postMessage support for site title and description.
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
@@ -286,6 +64,10 @@ function smartline_customize_register_options( $wp_customize ) {
 	// Change default background section
 	$wp_customize->get_control( 'background_color'  )->section   = 'background_image';
 	$wp_customize->get_section( 'background_image'  )->title     = 'Background';
+	
+	// Change Featured Content Section
+	$wp_customize->get_section( 'featured_content'  )->panel = 'smartline_options_panel';
+	$wp_customize->get_section( 'featured_content'  )->priority = 40;
 	
 	// Add Header Tagline option
 	$wp_customize->add_setting( 'smartline_theme_options[header_tagline]', array(
