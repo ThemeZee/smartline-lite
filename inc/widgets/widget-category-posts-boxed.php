f00c58377f566e78cdc563c1397148f373f8e61e
Thomas W <netzberufler@googlemail.com>
2015-03-18 10:13:32 +0100

    changed category dropdown setting to show empty categories

--- inc/widgets/widget-category-posts-boxed.php
+++ inc/widgets/widget-category-posts-boxed.php
@@ -280,6 +280,7 @@ class Smartline_Category_Posts_Boxed_Widget extends WP_Widget {
 				$args = array(
 					'show_option_all'    => __('All Categories', 'smartline-lite'),
 					'show_count' 		 => true,
+					'hide_empty'		 => false,
 					'selected'           => $category,
 					'name'               => $this->get_field_name('category'),
 					'id'                 => $this->get_field_id('category')
