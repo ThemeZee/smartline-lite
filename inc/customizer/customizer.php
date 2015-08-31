ad0954c449fa6b36802281423db80af85c42a014
Thomas W <netzberufler@googlemail.com>
2015-08-31 20:17:43 +0200

    made background section translation-ready

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -36,7 +36,7 @@ function smartline_customize_register_options( $wp_customize ) {
 	
 	// Change default background section
 	$wp_customize->get_control( 'background_color'  )->section   = 'background_image';
-	$wp_customize->get_section( 'background_image'  )->title     = 'Background';
+	$wp_customize->get_section( 'background_image'  )->title     = __( 'Background', 'smartline-lite' );
 	
 	// Add Header Tagline option
 	$wp_customize->add_setting( 'smartline_theme_options[header_tagline]', array(
